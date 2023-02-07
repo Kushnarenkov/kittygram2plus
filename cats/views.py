19ee877e6f20162f115760ee141e32b3e09ae9e7
Илья Кушнаренков <R3Mly@yandex.ru>
2023-02-07 14:55:22 +0400

    Add ordering

--- cats/views.py
+++ cats/views.py
@@ -1,6 +1,7 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, filters
 from rest_framework.throttling import ScopedRateThrottle
 from rest_framework.pagination import LimitOffsetPagination
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Achievement, Cat, User
 from .permissions import OwnerOrReadOnly, ReadOnly
@@ -14,22 +15,47 @@ class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
     permission_classes = (OwnerOrReadOnly,) # подключаем разрешения
-    # Если кастомный тротлинг-класс вернёт True - запросы будут обработаны
-    # Если он вернёт False - все запросы будут отклонены
+        # Если кастомный тротлинг-класс вернёт True - запросы будут обработаны
+        # Если он вернёт False - все запросы будут отклонены
     throttle_classes = (WorkingHoursRateThrottle, ScopedRateThrottle) # Подключили класс AnonRateThrottle
-    # А далее применится лимит low_request
-    # Для любых пользователей установим кастомный лимит 1 запрос в минуту
+        # А далее применится лимит low_request
+        # Для любых пользователей установим кастомный лимит 1 запрос в минуту
     throttle_scope = 'low_request'
-    # Для PageNumberPagination, установленного в классе, параметр PAGE_SIZE 
-    # будет взят из словаря REST_FRAMEWORK в settings.py.
-    # Если пагинация установлена на уровне проекта, то для отдельного класса 
-    # её можно отключить, установив для атрибута pagination_class значение None.
+        # Указываем фильтрующий бэкенд DjangoFilterBackend
+        # Из библиотеки django-filter
+    filter_backends = (
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+        )
+        # Для PageNumberPagination, установленного в классе, параметр PAGE_SIZE 
+        # будет взят из словаря REST_FRAMEWORK в settings.py.
+        # Если пагинация установлена на уровне проекта, то для отдельного класса 
+        # её можно отключить, установив для атрибута pagination_class значение None.
     # pagination_class = PageNumberPagination
-    # Даже если на уровне проекта установлен PageNumberPagination
-    # Для котиков будет работать LimitOffsetPagination
+        # Даже если на уровне проекта установлен PageNumberPagination
+        # Для котиков будет работать LimitOffsetPagination
     # pagination_class = LimitOffsetPagination
-    # Вот он наш собственный класс пагинации с page_size=20
-    pagination_class = CatsPagination
+        # Вот он наш собственный класс пагинации с page_size=20
+        # Временно отключим пагинацию на уровне вьюсета, 
+        # так будет удобнее настраивать фильтрацию
+    # pagination_class = CatsPagination
+    pagination_class = None
+        # Фильтровать будем по полям color и birth_year модели Cat
+    filterset_fields = ('color', 'birth_year',)
+    search_fields = ('name',)
+    ordering_fileds = ('name', 'birth_year',)
+    ordering = ('birth_year',)
+
+    # def get_queryset(self):
+        # queryset = Cat.objects.all()
+            # Добыть параметр color из GET-запроса
+        # color = self.request.query_params.get['color']
+        # if color is not None:
+                # Через ORM отфильтровать объекты модели Cat
+                # по значению параметра color, полученного в запросе
+            # queryset = queryset.filter(color=color)
+        # return queryset
 
     def get_permissoins(self):
         # Если в GET-запросе требуется получить информацию об объекте
